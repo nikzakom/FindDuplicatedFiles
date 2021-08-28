d969e9531e877c3d639be265f788e6b10d509998
nikzakom <nikzakom@gmail.com>
2021-08-28 12:59:30 +0300

    initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace FindDuplicatedFiles
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFirstFolderBtn = new System.Windows.Forms.Button();
+            this.selectSecondFolderBtn = new System.Windows.Forms.Button();
+            this.firstFolderPathTB = new System.Windows.Forms.TextBox();
+            this.secondFolderPathTB = new System.Windows.Forms.TextBox();
+            this.logTB = new System.Windows.Forms.TextBox();
+            this.firstFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.secondFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // selectFirstFolderBtn
+            // 
+            this.selectFirstFolderBtn.Location = new System.Drawing.Point(468, 12);
+            this.selectFirstFolderBtn.Name = "selectFirstFolderBtn";
+            this.selectFirstFolderBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectFirstFolderBtn.TabIndex = 0;
+            this.selectFirstFolderBtn.Text = "Browse";
+            this.selectFirstFolderBtn.UseVisualStyleBackColor = true;
+            this.selectFirstFolderBtn.Click += new System.EventHandler(this.SelectFirstFolderBtn_Click);
+            // 
+            // selectSecondFolderBtn
+            // 
+            this.selectSecondFolderBtn.Location = new System.Drawing.Point(468, 53);
+            this.selectSecondFolderBtn.Name = "selectSecondFolderBtn";
+            this.selectSecondFolderBtn.Size = new System.Drawing.Size(75, 22);
+            this.selectSecondFolderBtn.TabIndex = 1;
+            this.selectSecondFolderBtn.Text = "Browse";
+            this.selectSecondFolderBtn.UseVisualStyleBackColor = true;
+            this.selectSecondFolderBtn.Click += new System.EventHandler(this.SelectSecondFolderBtn_Click);
+            // 
+            // firstFolderPathTB
+            // 
+            this.firstFolderPathTB.Location = new System.Drawing.Point(12, 12);
+            this.firstFolderPathTB.Name = "firstFolderPathTB";
+            this.firstFolderPathTB.ReadOnly = true;
+            this.firstFolderPathTB.Size = new System.Drawing.Size(439, 23);
+            this.firstFolderPathTB.TabIndex = 2;
+            this.firstFolderPathTB.Text = "Select folder";
+            // 
+            // secondFolderPathTB
+            // 
+            this.secondFolderPathTB.Location = new System.Drawing.Point(12, 52);
+            this.secondFolderPathTB.Name = "secondFolderPathTB";
+            this.secondFolderPathTB.ReadOnly = true;
+            this.secondFolderPathTB.Size = new System.Drawing.Size(439, 23);
+            this.secondFolderPathTB.TabIndex = 3;
+            this.secondFolderPathTB.Text = "Select folder";
+            // 
+            // logTB
+            // 
+            this.logTB.Location = new System.Drawing.Point(12, 95);
+            this.logTB.Multiline = true;
+            this.logTB.Name = "logTB";
+            this.logTB.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.logTB.Size = new System.Drawing.Size(531, 222);
+            this.logTB.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 329);
+            this.Controls.Add(this.logTB);
+            this.Controls.Add(this.secondFolderPathTB);
+            this.Controls.Add(this.firstFolderPathTB);
+            this.Controls.Add(this.selectSecondFolderBtn);
+            this.Controls.Add(this.selectFirstFolderBtn);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Find Duplicated Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFirstFolderBtn;
+        private System.Windows.Forms.Button selectSecondFolderBtn;
+        private System.Windows.Forms.TextBox firstFolderPathTB;
+        private System.Windows.Forms.TextBox secondFolderPathTB;
+        private System.Windows.Forms.TextBox logTB;
+        private System.Windows.Forms.FolderBrowserDialog firstFolderBrowser;
+        private System.Windows.Forms.FolderBrowserDialog secondFolderBrowser;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindDuplicatedFiles
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //private string outputFilePath = Application.StartupPath;
+
+        private string firstFolderPath = null;
+        private string secondFolderPath = null;
+
+        // Full filenames (with path)
+        private List<string> firstFolderFilePaths;
+        private List<string> secondFolderFilePaths;
+
+        private List<string> duplicatedFiles;
+
+        private List<string> GetFiles(string folderPath)
+        {
+            if (!(Directory.Exists(folderPath)))
+            {
+                MessageBox.Show("Selected directory not found!", "Error");
+                return null;
+            }
+
+            int filesCounter = 0;
+            List<string> files = new List<string>();
+
+            IEnumerable<string> rootFiles = Directory.EnumerateFiles(folderPath, "*", SearchOption.TopDirectoryOnly);
+
+            // At first we find all files in root directory
+            foreach (string f in rootFiles)
+            {
+                //logTB.Text += Environment.NewLine + "Root folder: " + f.Substring(folderPath.Length + 1) + Environment.NewLine;
+                //logTB.Text += Environment.NewLine + f.Substring(folderPath.Length + 1) + Environment.NewLine;
+                //PrintFileName(f);
+
+                files.Add(f);
+                filesCounter++;
+            }
+
+            //logTB.Text += Environment.NewLine;
+
+            IEnumerable<string> directories = Directory.EnumerateDirectories(folderPath, "*", SearchOption.AllDirectories);
+
+            // Next we find files from subfolders
+            foreach (string dir in directories)
+            {
+                //logTB.Text += "Subfolder: " + dir + Environment.NewLine;
+                IEnumerable<string> subfolderFiles = Directory.EnumerateFiles(dir, "*", SearchOption.TopDirectoryOnly);
+
+                foreach (string f in subfolderFiles)
+                {
+                    //logTB.Text += f.Substring(dir.Length + 1) + Environment.NewLine;
+                    //PrintFileName(f, dir);
+
+                    files.Add(f);
+                    filesCounter++;
+                }
+                //logTB.Text += Environment.NewLine;
+            }
+
+            logTB.Text += "Total files: " + filesCounter.ToString() + Environment.NewLine + "-----";
+            logTB.Text += Environment.NewLine;
+
+            //PrintFileList(files);
+
+            return files;
+        }
+
+        private void PrintFileList(List<string> list)
+        {
+            foreach (var i in list)
+            {
+                logTB.Text += i;
+                logTB.Text += Environment.NewLine;
+            }
+        }
+
+        // Leave just filename + extension
+        private string GetFileName(string filePath)
+        {
+            string fileName = string.Empty;
+
+            int ind = filePath.LastIndexOf(@"\");
+
+            if (ind > 0)
+            {
+                fileName = filePath.Substring(ind + 1);
+            }
+       
+            return fileName;
+        }
+
+        private void SelectFirstFolderBtn_Click(object sender, EventArgs e)
+        {
+            // Clean output on each selecting first folder
+            logTB.Text = string.Empty;
+
+            firstFolderBrowser.ShowDialog();
+
+            // If user doesn't select any folder and closed dialog
+            if (firstFolderBrowser.@SelectedPath == string.Empty)
+            {
+                return;
+            }
+
+            firstFolderPath = firstFolderBrowser.@SelectedPath;
+
+            logTB.Text += "First selected folder: " + firstFolderPath + Environment.NewLine;
+
+            firstFolderFilePaths = GetFiles(firstFolderPath);
+
+            firstFolderPathTB.Text = firstFolderPath;
+        }
+
+        private void SelectSecondFolderBtn_Click(object sender, EventArgs e)
+        {
+            // If first folder wasn't selected we restrict selecting second folder
+            if (firstFolderBrowser.@SelectedPath == string.Empty)
+            {
+                MessageBox.Show("Please select first folder before selecting second one!", "Error");
+                return;
+            }
+
+            secondFolderBrowser.ShowDialog();
+
+            if (secondFolderBrowser.@SelectedPath == string.Empty)
+            {
+                return;
+            }
+
+            secondFolderPath = secondFolderBrowser.@SelectedPath;
+
+            logTB.Text += "Second selected folder: " + secondFolderPath + Environment.NewLine;
+
+            secondFolderFilePaths = GetFiles(secondFolderPath);
+
+            secondFolderPathTB.Text = secondFolderPath;
+
+            FindDuplicatedFiles(firstFolderFilePaths, secondFolderFilePaths);
+        }
+
+        private void FindDuplicatedFiles(List<string> firstFileList, List<string> secondFileList)
+        {
+            duplicatedFiles = new List<string>();
+
+            string f1;
+            string f2;
+
+            foreach (var i in firstFileList)
+            {
+                f1 = GetFileName(i);
+
+                foreach (var j in secondFileList)
+                {
+                    f2 = GetFileName(j);
+
+                    if (f1 == f2)
+                    {
+                        duplicatedFiles.Add(j);
+                        break;
+                    }
+                }
+            }
+
+            logTB.Text += "Duplicates found: " + duplicatedFiles.Count + Environment.NewLine;
+            logTB.Text += "Duplicated files:" + Environment.NewLine;
+
+            PrintFileList(duplicatedFiles);
+
+        }
+
+        //private void OpenOutputFile()
+        //{
+        //    if (File.Exists(outputFilePath))
+        //    {
+        //        Process.Start(outputFilePath);
+        //    }
+        //}
+
+        //private void WriteToFile(List<string> data)
+        //{
+        //    if (!File.Exists(outputFilePath))
+        //    {
+        //        File.Create(outputFilePath);
+        //    }
+
+        //    try
+        //    {
+        //        File.WriteAllLines(outputFilePath, data);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.ToString());
+        //    }
+        //}
+    }
+}
